5247a8d6e9311bf016dff864252ff7b390c06cc0
jeyranjahanbakhsh <jahanbakhshjeyran885@gmail.com>
2025-12-17 16:21:53 +0330

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            passname n1 = new passname();
+            n1.name = Console.ReadLine();
+            n1.pass = Console.ReadLine();
+
+            n1.method();
+            Console.ReadKey();
+        }
+    }
+    class passname
+    {
+        public string name = "";
+        public string pass = "";
+
+
+        public void method()
+        {
+            if (name == "admin" && pass == "123")
+                Console.Write("true");
+            else
+                Console.WriteLine("false");
+        }
+    }
+}
